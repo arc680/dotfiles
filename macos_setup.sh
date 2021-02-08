74a602aec888539817d54277be0bcaf2615eceb2
arc680 <arc680@live.com>
2021-02-09 02:06:01 +0900

    add setup script

---
+++ macos_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install command-line-tools
+if [[ ! -d /Library/Developer/CommandLineTools ]]; then
+    echo "Installing command-line-tools"
+    xcode-select --install
+fi
+
+# install Homebrew
+if test ! $(which brew); then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew tap homebrew/bundle
+    brew tap caskroom/cask
+fi
---
+++ setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+DOT=~/dotfiles; export DOT
+DOT_GITHUB="git@github.com:arc680/dotfiles.git"; export DOT_GITHUB
+ZPREZTO_GITHUB="https://github.com/sorin-ionescu/prezto.git"; export ZPREZTO_GITHUB
+case "$(uname)" in
+    "Darwin")
+        DOT_FILES=(
+        .gitconfig
+        .gitconfig.local
+        .vimrc
+        .zpreztorc
+        .zprofile
+        .zshrc
+        .zshrc.base
+        .zshrc.macos
+        ); export DOT_FILES;;
+    "Linux")
+        DOT_FILES=(
+        .gitconfig
+        .gitconfig.local
+        .vimrc
+        .zpreztorc
+        .zprofile
+        .zshrc
+        .zshrc.base
+        .zshrc.linux
+        ); export DOT_FILES;;
+    *)
+        DOT_FILES=(
+        .gitconfig
+        .gitconfig.local
+        .vimrc
+        .zpreztorc
+        .zprofile
+        .zshrc
+        .zshrc.base
+        ); export DOT_FILES
+esac
+
+# install zprezto
+git clone --recursive ${ZPREZTO_GITHUB} "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# install dotfiles
+git clone ${DOT_GITHUB} ${DOT}
+for file in ${DOT_FILES[@]}
+do
+    yes | ln -s ${DOT}/${file} ~/${file}
+done
+
+echo "Complete: please restart Terminal and continue manual settings"
+
